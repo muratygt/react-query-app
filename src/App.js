dd5f2df1f8320948005c35659784ed33bcdb3786
Matt <muratygt@icloud.com>
2020-10-18 02:02:08 -0400

    React Query loading and error state implementation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useQuery } from 'react-query';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Hello
-      </header>
-    </div>
+  const queryInfo = useQuery('pokemon', async () => { 
+    await new Promise(resolve => setTimeout(resolve, 1000))
+
+      if(true) {
+        throw new Error('Testing Error State');
+      }
+
+      axios
+      .get('https://pokeapi.co/api/v2/pokemon')
+      .then(res => res.data.results)
+    }
+  )
+  console.log(queryInfo);
+  return queryInfo.isLoading ? (
+    'Loading'
+	) : queryInfo.isError ? 
+	queryInfo.error.message : (
+	<div className="App">
+	{queryInfo.data.map(result => <div key={result.name}>{result.name}</div>)}
+	</div>
   );
 }
 
